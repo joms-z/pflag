b6813ae2f498ca78797e6e064388917312678ba0
Jyotheeswar Arvind <jyotheeshwar@gmail.com>
2016-10-22 03:49:15 -0400

    views, ctrls, and routes added

--- pflag_ui/app/scripts/app.js
+++ pflag_ui/app/scripts/app.js
@@ -20,19 +20,59 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        templateUrl: 'views/init.html',
+        controller: 'InitCtrl',
+        controllerAs: 'init'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .when('/chat', {
+        templateUrl: 'views/chat.html',
+        controller: 'ChatCtrl',
+        controllerAs: 'chat'
+      })
+      .when('/choose', {
+        templateUrl: 'views/choose.html',
+        controller: 'ChooseCtrl',
+        controllerAs: 'choose'
       })
       .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl',
         controllerAs: 'login'
+      })
+      .when('/message', {
+        templateUrl: 'views/message.html',
+        controller: 'MessageCtrl',
+        controllerAs: 'message'
+      })
+      .when('/msg', {
+        templateUrl: 'views/msg.html',
+        controller: 'MsgCtrl',
+        controllerAs: 'msg'
+      })
+      .when('/no_one', {
+        templateUrl: 'views/no_one.html',
+        controller: 'NoOneCtrl',
+        controllerAs: 'noOne'
+      })
+      .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl',
+        controllerAs: 'profile'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignUpCtrl',
+        controllerAs: 'signUp'
+      })
+      .when('/sorry', {
+        templateUrl: 'views/sorry.html',
+        controller: 'SorryCtrl',
+        controllerAs: 'sorry'
+      })
+      .when('/trying', {
+        templateUrl: 'views/trying.html',
+        controller: 'TryingCtrl',
+        controllerAs: 'trying'
       })
       .otherwise({
         redirectTo: '/'
