3193e1fa3357c5f3a1647c5ebd04a2a87bce7cf6
Jyotheeswar Arvind <jyotheeshwar@gmail.com>
2016-10-22 01:30:24 -0400

    angular scaffolded

---
+++ pflag_ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pflagUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pflagUiApp
+ */
+angular.module('pflagUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
