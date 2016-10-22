a1894c7f60ee83100340d1b2bf2d19df6c55ca49
Jyotheeswar Arvind <jyotheeshwar@gmail.com>
2016-10-22 05:55:25 -0400

    server added

--- pflag_ui/app/scripts/controllers/login.js
+++ pflag_ui/app/scripts/controllers/login.js
@@ -8,6 +8,23 @@
  * Controller of the pflagUiApp
  */
 angular.module('pflagUiApp')
-  .controller('LoginCtrl', function () {
+  .controller('LoginCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+
+  	$scope.username = "";
+  	$scope.password = "";
+
+  	$scope.loginFunction = function () {
+  		var data = {
+  			username: $scope.username,
+  			password: $scope.password
+  		};
+
+  		$http.post('/login', data).success(function (data, status) {
+    		console.log('Logged in successfully');
+            $location.path('/chat');
+    	}).error(function (data, status) {
+    		console.log('FAILED WITH DATA: ' + data + ' STATUS: ' + status);
+    	});
+  	};
     
-  });
+  }]);
